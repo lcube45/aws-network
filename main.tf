934003bd2a53d6ec080f31e5ba330de8217a966d
lcube <info@lcube.fr>
2024-05-15 23:32:05 +0200

    tf plan in another account

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+  alias = "sandbox"
+
+  assume_role {
+    role_arn = "arn:aws:iam::966616891605:role/Devops"
+  }
+}
+
+resource "aws_s3_bucket" "demo" {
+  provider = aws.sandbox
+  bucket = "lcube-tf-example-terraform-bucket-dncbdhddn"
+}
